c7b2292e4d940e6dbdfbdf6d7aad830fd13ee98f
haden <80521474+thdefn@users.noreply.github.com>
2023-02-25 14:07:19 +0900

    feat : 잔액 사용

---
+++ src/main/java/com/example/account/service/TransactionService.java
@@ -0,0 +1,99 @@
+package com.example.account.service;
+
+import com.example.account.domain.Account;
+import com.example.account.domain.AccountUser;
+import com.example.account.domain.Transaction;
+import com.example.account.dto.TransactionDto;
+import com.example.account.exception.AccountException;
+import com.example.account.repository.AccountRepository;
+import com.example.account.repository.AccountUserRepository;
+import com.example.account.repository.TransactionRepository;
+import com.example.account.type.AccountStatus;
+import com.example.account.type.ErrorCode;
+import com.example.account.type.TransactionResultType;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+import static com.example.account.type.ErrorCode.*;
+import static com.example.account.type.TransactionResultType.F;
+import static com.example.account.type.TransactionResultType.S;
+import static com.example.account.type.TransactionType.USE;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class TransactionService {
+    private final TransactionRepository transactionRepository;
+    private final AccountUserRepository accountUserRepository;
+    private final AccountRepository accountRepository;
+
+    /**
+     * 사용자가 없는 경우, 사용자 아이디와 계좌 소유주가 다른 경우
+     * 계좌가 이미 해지 상태인 경우, 거래 금액이 잔액보다 큰 경우
+     * 거래 금액이 너무 작거나 큰 경우 실패 응답
+     */
+    @Transactional // 1. 2. 는 동시에 일어나거나 둘 다 일어나지 않거나 서비스 쪽에 있는 코드들은 기본적으로 Transactional을 달아주자
+    public TransactionDto useBalance(Long userId, String accountNumber, Long amount) {
+        AccountUser user = accountUserRepository.findById(userId)
+                .orElseThrow(() -> new AccountException(USER_NOT_FOUND));
+
+        Account account = accountRepository.findByAccountNumber(accountNumber)
+                .orElseThrow(() -> new AccountException(ACCOUNT_NOT_FOUND));
+
+        validateUseBalance(user, account, amount);
+
+        // 1. account 테이블 잔액 변경
+        account.useBalance(amount); //balance를 변경하는 로직은 엔티티 안에 넣어주는 것이 좋음
+
+        // 2. transaction 테이블에 insert
+        return TransactionDto.fromEntity(
+                saveAndGetTransaction(S, account, amount)
+        );
+    }
+
+    private void validateUseBalance(AccountUser user, Account account, Long amount) {
+        if (!Objects.equals(user.getId(), account.getAccountUser().getId())) { //Objects.equals(null,null) > true
+            throw new AccountException(USER_ACCOUNT_UNMATCH);
+        }
+
+        if (account.getAccountStatus() != AccountStatus.IN_USE) {
+            throw new AccountException(ACCOUNT_ALREADY_UNREGISTERED);
+        }
+
+        if (account.getBalance() < amount) {
+            throw new AccountException(AMOUNT_EXCEED_BALANCE);
+        }
+    }
+
+    @Transactional
+    public void saveFailedUseTransaction(String accountNumber, Long amount) {
+        Account account = accountRepository.findByAccountNumber(accountNumber)
+                .orElseThrow(()->new AccountException(ACCOUNT_NOT_FOUND));
+
+        saveAndGetTransaction(F, account, amount);
+    }
+
+    //코드 중복 최소화하면서 저장하는 부분 공통화
+    private Transaction saveAndGetTransaction(
+            TransactionResultType transactionResultType,
+            Account account,
+            Long amount) {
+        return transactionRepository.save(
+                Transaction.builder()
+                        .transactionType(USE)
+                        .transactionResultType(transactionResultType)
+                        .account(account)
+                        .amount(amount)
+                        .balanceSnapshot(account.getBalance())
+                        .transactionId(UUID.randomUUID().toString().replace("-", "")) //고유한 값으로 UUID 사용 -만 없애줌, UUID인 걸 비밀로 하기 위함
+                        .transactedAt(LocalDateTime.now())
+                        .build()
+        );
+    }
+}
